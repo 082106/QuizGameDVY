945082aac140660cb92fe8e1701a79a5098be780
082106 <desislawjordanov@gmail.com>
2023-07-06 13:29:46 +0300

    cosmetic only

--- src/Main.java
+++ src/Main.java
@@ -13,62 +13,46 @@ public class Main {
         List<String> categories = Arrays.asList("Geography", "Art", "Literature");
         Random random = new Random();
         Scanner scanner = new Scanner(System.in);
-
         System.out.println("Welcome to the Quiz");
         System.out.println("Choose a category:");
         for (int i = 0; i < categories.size(); i++) {
             System.out.println((i + 1) + ". " + categories.get(i));
         }
-
         int selectedCategoryIndex = scanner.nextInt();
         scanner.nextLine();
-
         if (selectedCategoryIndex < 1 || selectedCategoryIndex > categories.size()) {
             System.out.println("There is no such category. Exiting the game.");
             return;
         }
-
         String selectedCategory = categories.get(selectedCategoryIndex - 1);
         List<String[]> questions = readQuestionsFromCSV(selectedCategory, random);
-
         if (questions.isEmpty()) {
             System.out.println("We forgot to put questions in this category :(. Exiting the game.");
             return;
         }
-
         System.out.println("Let's start the quiz!");
-
         int score = 0;
         int questionIndex = 0;
-
         while (questionIndex < questions.size()) {
             String[] questionData = questions.get(questionIndex);
-
             String questionText = questionData[0];
             String correctAnswer = questionData[1];
             String[] wrongAnswers = Arrays.copyOfRange(questionData, 2, questionData.length);
-
             System.out.println("Question: " + questionText);
-
             List<String> answerOptions = new ArrayList<>();
             answerOptions.add(correctAnswer);
             answerOptions.addAll(Arrays.asList(wrongAnswers));
             answerOptions = shuffleList(answerOptions, random);
-
             for (int i = 0; i < answerOptions.size(); i++) {
                 System.out.println((i + 1) + ". " + answerOptions.get(i));
             }
-
             int userAnswerIndex = scanner.nextInt();
-            scanner.nextLine(); // Consume the newline character
-
+            scanner.nextLine();
             if (userAnswerIndex < 1 || userAnswerIndex > answerOptions.size()) {
                 System.out.println("Invalid selection. Game over.");
                 break;
             }
-
             String userAnswer = answerOptions.get(userAnswerIndex - 1);
-
             if (userAnswer.equals(correctAnswer)) {
                 System.out.println("Correct answer!");
                 score++;
@@ -78,13 +62,11 @@ public class Main {
                 break;
             }
         }
-
         System.out.println("Your final score: " + score);
     }
 
     private static List<String[]> readQuestionsFromCSV(String category, Random random) {
         List<String[]> questions = new ArrayList<>();
-
         try (BufferedReader br = new BufferedReader(new FileReader("questions.csv"))) {
             String line;
             while ((line = br.readLine()) != null) {
@@ -97,7 +79,6 @@ public class Main {
         } catch (IOException e) {
             e.printStackTrace();
         }
-
         Collections.shuffle(questions, random);
         return questions;
     }
