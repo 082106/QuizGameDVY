941fb64a45d3f751b66bfbd5541e76e0d4a2c635
082106 <desislawjordanov@gmail.com>
2023-07-06 13:56:56 +0300

    cosmetic only 2

--- src/Main.java
+++ src/Main.java
@@ -9,28 +9,56 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
+    public static final String ANSI_RESET = "\u001B[0m";
+    public static final String ANSI_RED = "\u001B[31m";
+    public static final String ANSI_GREEN = "\u001B[32m";
+    public static final String ANSI_YELLOW = "\u001B[33m";
+    public static final String ANSI_BLUE = "\u001B[34m";
+    public static final String ANSI_PURPLE = "\u001B[35m";
+    public static final String ANSI_CYAN = "\u001B[36m";
+    
+    private static Random random = new Random();
+    private static Scanner scanner = new Scanner(System.in);
+    
     public static void main(String[] args) {
         List<String> categories = Arrays.asList("Geography", "Art", "Literature");
-        Random random = new Random();
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Welcome to the Quiz");
+        
+        System.out.println(ANSI_CYAN + "Welcome to the Quiz" + ANSI_RESET);
+        int selectedCategoryIndex = getSelectedCategoryIndex(categories);
+        if (selectedCategoryIndex == -1) {
+            System.out.println(ANSI_RED + "There is no such category. Exiting the game." + ANSI_RESET);
+            return;
+        }
+        
+        String selectedCategory = categories.get(selectedCategoryIndex);
+        List<String[]> questions = readQuestionsFromCSV(selectedCategory);
+        
+        if (questions.isEmpty()) {
+            System.out.println(ANSI_RED + "We forgot to put questions in this category :(. Exiting the game." + ANSI_RESET);
+            return;
+        }
+        
+        System.out.println("Let's start the quiz!");
+        int score = playQuiz(questions);
+        System.out.println("Your final score: " + score);
+    }
+    
+    private static int getSelectedCategoryIndex(List<String> categories) {
         System.out.println("Choose a category:");
         for (int i = 0; i < categories.size(); i++) {
-            System.out.println((i + 1) + ". " + categories.get(i));
+            System.out.println((i + 1) + ". " + getRainbowText(categories.get(i)));
         }
+        
         int selectedCategoryIndex = scanner.nextInt();
         scanner.nextLine();
         if (selectedCategoryIndex < 1 || selectedCategoryIndex > categories.size()) {
-            System.out.println("There is no such category. Exiting the game.");
-            return;
+            return -1;
         }
-        String selectedCategory = categories.get(selectedCategoryIndex - 1);
-        List<String[]> questions = readQuestionsFromCSV(selectedCategory, random);
-        if (questions.isEmpty()) {
-            System.out.println("We forgot to put questions in this category :(. Exiting the game.");
-            return;
-        }
-        System.out.println("Let's start the quiz!");
+        
+        return selectedCategoryIndex - 1;
+    }
+    
+    private static int playQuiz(List<String[]> questions) {
         int score = 0;
         int questionIndex = 0;
         while (questionIndex < questions.size()) {
@@ -38,34 +66,30 @@ public class Main {
             String questionText = questionData[0];
             String correctAnswer = questionData[1];
             String[] wrongAnswers = Arrays.copyOfRange(questionData, 2, questionData.length);
-            System.out.println("Question: " + questionText);
-            List<String> answerOptions = new ArrayList<>();
-            answerOptions.add(correctAnswer);
-            answerOptions.addAll(Arrays.asList(wrongAnswers));
-            answerOptions = shuffleList(answerOptions, random);
-            for (int i = 0; i < answerOptions.size(); i++) {
-                System.out.println((i + 1) + ". " + answerOptions.get(i));
-            }
-            int userAnswerIndex = scanner.nextInt();
-            scanner.nextLine();
+            System.out.println("Question: " + getRainbowText(questionText));
+            List<String> answerOptions = getShuffledAnswerOptions(correctAnswer, wrongAnswers);
+            printAnswerOptions(answerOptions);
+            
+            int userAnswerIndex = getUserAnswerIndex(answerOptions.size());
             if (userAnswerIndex < 1 || userAnswerIndex > answerOptions.size()) {
-                System.out.println("Invalid selection. Game over.");
+                System.out.println(ANSI_RED + "Invalid selection. Game over." + ANSI_RESET);
                 break;
             }
+            
             String userAnswer = answerOptions.get(userAnswerIndex - 1);
             if (userAnswer.equals(correctAnswer)) {
-                System.out.println("Correct answer!");
+                System.out.println(ANSI_GREEN + "Correct answer!" + ANSI_RESET);
                 score++;
                 questionIndex++;
             } else {
-                System.out.println("Wrong answer. Game over.");
+                System.out.println(ANSI_RED + "Wrong answer. Game over." + ANSI_RESET);
                 break;
             }
         }
-        System.out.println("Your final score: " + score);
+        return score;
     }
-
-    private static List<String[]> readQuestionsFromCSV(String category, Random random) {
+    
+    private static List<String[]> readQuestionsFromCSV(String category) {
         List<String[]> questions = new ArrayList<>();
         try (BufferedReader br = new BufferedReader(new FileReader("questions.csv"))) {
             String line;
@@ -82,10 +106,42 @@ public class Main {
         Collections.shuffle(questions, random);
         return questions;
     }
-
-    private static List<String> shuffleList(List<String> list, Random random) {
+    
+    private static List<String> getShuffledAnswerOptions(String correctAnswer, String[] wrongAnswers) {
+        List<String> answerOptions = new ArrayList<>();
+        answerOptions.add(correctAnswer);
+        answerOptions.addAll(Arrays.asList(wrongAnswers));
+        return shuffleList(answerOptions);
+    }
+    
+    private static List<String> shuffleList(List<String> list) {
         List<String> shuffledList = new ArrayList<>(list);
         Collections.shuffle(shuffledList, random);
         return shuffledList;
+    }
+    
+    private static void printAnswerOptions(List<String> answerOptions) {
+        for (int i = 0; i < answerOptions.size(); i++) {
+            System.out.println((i + 1) + ". " + getRainbowText(answerOptions.get(i)));
+        }
+    }
+    
+    private static int getUserAnswerIndex(int maxIndex) {
+        int userAnswerIndex = scanner.nextInt();
+        scanner.nextLine();
+        return userAnswerIndex;
+    }
+    
+    private static String getRainbowText(String text) {
+        StringBuilder rainbowText = new StringBuilder();
+        String[] rainbowColors = { ANSI_RED, ANSI_YELLOW, ANSI_GREEN, ANSI_CYAN, ANSI_BLUE, ANSI_PURPLE };
+        int colorIndex = 0;
+        
+        for (int i = 0; i < text.length(); i++) {
+            rainbowText.append(rainbowColors[colorIndex]).append(text.charAt(i));
+            colorIndex = (colorIndex + 1) % rainbowColors.length;
+        }
+        
+        return rainbowText.toString();
     }
 }
